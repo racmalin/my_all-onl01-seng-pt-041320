7389b1457e4c70af89b8f8d3cbefc04f3a9fe515
Kevin C Malin <racmalin@yahoo.com>
2020-04-26 22:51:17 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -8,11 +8,11 @@ def my_all?(collection)
 yield(collection[i])
    i = i + 1 
   end
- 
-  if block_return_values.include?
-(false)
-    false
-  else
-    true
-  end
-end
+end 
+#   if block_return_values.include?
+# (false)
+#     false
+#   else
+#     true
+#   end
+# end
